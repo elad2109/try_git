a90f9973bfb690ea99f8a88dcab39ba73aecb82b
elad2109 <elad2109@gmail.com>
2022-11-27 21:31:02 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,1 +1,85 @@
+import numpy as np
+import os
+import pickle
+import streamlit as st
+import sys
+import tensorflow as tf
+import urllib
 
+sys.path.append("tl_gan")
+sys.path.append("pg_gan")
+import feature_axis
+import tfutil
+import tfutil_cpu
+
+
+def main():
+    st.title("Streamlit Face-GAN Demo")
+    """This demo demonstrates  using [Nvidia's Progressive Growing of GANs](https://research.nvidia.com/publication/2017-10_Progressive-Growing-of) and 
+    Shaobo Guan's [Transparent Latent-space GAN method](https://blog.insightdatascience.com/generating-custom-photo-realistic-faces-using-ai-d170b1b59255) 
+    for tuning the output face's characteristics. For more information, check out the tutorial on [Towards Data Science](https://towardsdatascience.com/building-machine-learning-apps-with-streamlit-667cef3ff509)."""
+
+    # Download all data files if they aren't already in the working directory.
+    for filename in EXTERNAL_DEPENDENCIES.keys():
+        download_file(filename)
+
+    # Read in models from the data files.
+    tl_gan_model, feature_names = load_tl_gan_model()
+    session, pg_gan_model = load_pg_gan_model()
+
+    st.sidebar.title("Features")
+    seed = 27834096
+    # If the user doesn't want to select which features to control, these will be used.
+    default_control_features = ["Young", "Smiling", "Male"]
+
+    if st.sidebar.checkbox("Show advanced options"):
+        # Randomly initialize feature values.
+        features = get_random_features(feature_names, seed)
+
+        # Some features are badly calibrated and biased. Removing them
+        block_list = ["Attractive", "Big_Lips", "Big_Nose", "Pale_Skin"]
+        sanitized_features = [
+            feature for feature in features if feature not in block_list
+        ]
+
+        # Let the user pick which features to control with sliders.
+        control_features = st.sidebar.multiselect(
+            "Control which features?",
+            sorted(sanitized_features),
+            default_control_features,
+        )
+    else:
+        features = get_random_features(feature_names, seed)
+        # Don't let the user pick feature values to control.
+        control_features = default_control_features
+
+    # Insert user-controlled values from sliders into the feature vector.
+    for feature in control_features:
+        features[feature] = st.sidebar.slider(feature, 0, 100, 50, 5)
+
+    st.sidebar.title("Note")
+    st.sidebar.write(
+        """Playing with the sliders, you _will_ find **biases** that exist in this
+        model.
+        """
+    )
+    st.sidebar.write(
+        """For example, moving the `Smiling` slider can turn a face from masculine to
+        feminine or from lighter skin to darker. 
+        """
+    )
+    st.sidebar.write(
+        """Apps like these that allow you to visually inspect model inputs help you
+        find these biases so you can address them in your model _before_ it's put into
+        production.
+        """
+    )
+    st.sidebar.caption(f"Streamlit version `{st.__version__}`")
+
+    # Generate a new image from this feature vector (or retrieve it from the cache).
+    with session.as_default():
+        image_out = generate_image(
+            session, pg_gan_model, tl_gan_model, features, feature_names
+        )
+
+    st.image(image_out, use_column_width=True)
